89f2652ac2a57a7d63b15b4e03cba26aeae4513e
xed <ana1lphaon@gmail.com>
2025-01-20 23:45:41 +0200

    added new textures to tile manager

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -19,7 +19,7 @@ public class TileManager {
     public TileManager(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
 
-        tile = new Tile[10];
+        tile = new Tile[32];
         mapTileNum = new int[gamePanel.maxWorldMapRow][gamePanel.maxWorldMapCol];
 
         getTileImage();
@@ -27,12 +27,49 @@ public class TileManager {
     }
 
     public void getTileImage() {
-        setup(0, "grass", false);
-        setup(1, "wall", true);
-        setup(2, "water", true);
-        setup(3, "earth", false);
-        setup(4, "tree", true);
-        setup(5, "sand", false);
+        // grass
+        setup(0, "grass_1", false);
+        setup(1, "grass_2", false);
+       
+        // sand
+        setup(3, "sand_1", false);
+        setup(4, "sand_2", false);
+        setup(5, "sand_3", false);
+        setup(5, "sand_4", false);
+        setup(6, "sand_5", false);
+        setup(7, "sand_6", false);
+        setup(8, "sand_7", false);
+        setup(9, "sand_8", false);
+        setup(10, "sand_9", false);
+        setup(11, "sand_10", false);
+        setup(12, "sand_11", false);
+        setup(13, "sand_12", false);
+        setup(14, "sand_13", false);
+
+        // tree
+        setup(15, "tree", true);
+
+        // wall
+        setup(16, "wall", true);
+
+        // water
+        setup(17, "water_1", true);
+        setup(18, "water_2", true);
+        setup(19, "water_3", true);
+        setup(20, "water_4", true);
+        setup(21, "water_5", true);
+        setup(22, "water_6", true);
+        setup(23, "water_7", true);
+        setup(24, "water_8", true);
+        setup(25, "water_9", true);
+        setup(26, "water_10", true);
+        setup(27, "water_11", true);
+        setup(28, "water_12", true);
+        setup(29, "water_13", true);
+        setup(30, "water_14", true);
+
+        // earth
+        setup(31, "earth", false);
     }
 
     public void loadMap(String mapPath) {
